afb57865fad5a372b2d6269587a717234c3aa72d
Rogelio Morrell <molekilla@gmail.com>
2021-12-07 12:15:43 -0500

    web3

--- ethereum/migrations/3_onchain_metadata.js
+++ ethereum/migrations/3_onchain_metadata.js
@@ -8,20 +8,19 @@ module.exports = async function (deployer) {
     console.log(metadata);
     let contract = new web3.eth.Contract(metadata.abi);
 
-    contract = truffleContract(
+    contract =  (
       contract.deploy({
         data: metadata.bytecode,
         arguments: [],
       })
     );
 
-    contractD = await deployer.deploy(contract);
-    // const newContractInstance = await contract.send({
-    //     from: '0x32A21c1bB6E7C20F547e930b53dAC57f42cd25F6',
-    //     gas: 1500000,
-    //     gasPrice: '30000000000'
-    // })
-    // console.log('Contract deployed at address: ', newContractInstance.options.address)
+    const newContractInstance = await contract.send({
+        from: '0x32A21c1bB6E7C20F547e930b53dAC57f42cd25F6',
+        gas: 1500000,
+        gasPrice: '30000000000'
+    })
+    console.log('Contract deployed at address: ', newContractInstance.options.address)
   } catch (e) {
     console.log(e);
   }
