7a201b152fcf7709a111ca5a1f48b4d0c5b7096e
Kendall Kant <kantkendall@gmail.com>
2021-12-07 12:03:24 -0500

    wip onchain metadata migration

--- ethereum/migrations/3_onchain_metadata.js
+++ ethereum/migrations/3_onchain_metadata.js
@@ -1,25 +1,28 @@
-const fs = require('fs')
+const fs = require("fs");
+var truffleContract = require("@truffle/contract");
 // const Web3 = require('web3')
 
-const metadata =require('../build/contracts/OnchainMetadata.json')
+const metadata = require("../build/contracts/OnchainMetadata.json");
 module.exports = async function (deployer) {
-  try{
-    console.log(metadata)
-    let contract = new web3.eth.Contract(metadata.abi)
-      
-  
-    contract = contract.deploy({
-        data: metadata.bytecode,
-        arguments: []
-    })
+  try {
+    console.log(metadata);
+    let contract = new web3.eth.Contract(metadata.abi);
 
-    const newContractInstance = await contract.send({
-        from: '0x32A21c1bB6E7C20F547e930b53dAC57f42cd25F6',
-        gas: 1500000,
-        gasPrice: '30000000000'
-    })
-    console.log('Contract deployed at address: ', newContractInstance.options.address)
+    contract = truffleContract(
+      contract.deploy({
+        data: metadata.bytecode,
+        arguments: [],
+      })
+    );
+
+    contractD = await deployer.deploy(contract);
+    // const newContractInstance = await contract.send({
+    //     from: '0x32A21c1bB6E7C20F547e930b53dAC57f42cd25F6',
+    //     gas: 1500000,
+    //     gasPrice: '30000000000'
+    // })
+    // console.log('Contract deployed at address: ', newContractInstance.options.address)
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
-}
+};
